9e083e5da61935b4c2a91b48ff162211ea80b66f
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-12-13 12:43:09 +0600

    live url

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,5 +1,5 @@
-export const baseURL = "http://10.10.7.103:7001";
+// export const baseURL = "http://10.10.7.103:7001";
 
-// export const baseURL = "http://35.183.138.114:7001";
+export const baseURL = "http://35.183.138.114:7001";
 
 //hjsdfhksd
