07ac669e7d2de6be92a618546f63bf1c4df9e5cc
pranab56 <pronabhalder53@gmail.com>
2026-01-05 11:38:21 +0600

    all ok

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,5 +1,5 @@
-export const baseURL = "http://10.10.7.103:7001";
+// export const baseURL = "http://10.10.7.103:7001";
 
-// export const baseURL = "https://api.thecanuckmall.ca";
+export const baseURL = "https://asif7001.binarybards.online";
 
 //hjsdfhksd
